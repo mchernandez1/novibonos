6e804bb491412105e85d78e0af0cba9b1079d164
Maria Camila Hernandez <mc.hernandez1@uniandes.edu.co>
2018-10-05 22:06:48 -0500

    Commit 1

---
+++ server/main.js
@@ -0,0 +1,28 @@
+import { Meteor } from 'meteor/meteor';
+
+import '../imports/api/tasks.js';
+
+Meteor.startup(() => {
+  // code to run on server at startup
+});
+
+ServiceConfiguration.configurations.remove({
+    service: "facebook"
+});
+
+ServiceConfiguration.configurations.insert({
+    service: "facebook",
+    appId: '485612705270142',
+    secret: '544a8b563c1a5460c1a4221b9530803a'
+});
+
+Accounts.onCreateUser(function (options, user) {
+
+    if (!user.services.facebook) {
+        return user;
+    }
+    user.username = user.services.facebook.name;
+    user.emails = [{address: user.services.facebook.email}];
+
+    return user;
+});
